f6c7b5ff9759bbd29e98069409a14385516f101c
jakubgorowski <fotokubut@gmail.com>
2015-06-04 22:24:17 +0200

    init commit

---
+++ src/com/example/OKM/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.OKM;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
