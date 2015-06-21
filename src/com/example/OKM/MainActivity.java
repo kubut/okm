87785bfbb2a10478e2865c64e7f9119e71e81eb7
jakubgorowski <fotokubut@gmail.com>
2015-06-21 15:34:23 +0200

    MainScreen: Exit from app

--- src/com/example/OKM/MainActivity.java
+++ src/com/example/OKM/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.OKM;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+    }
+
+    public void exitAction(View v){
+        finish();
     }
 }
