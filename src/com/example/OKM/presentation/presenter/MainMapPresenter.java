e3bfd5538cc13322cf74f961f44d728b607f1803
jakubgorowski <fotokubut@gmail.com>
2015-07-17 23:57:29 +0200

    change mainScreen to map, added DDD project structure

---
+++ src/com/example/OKM/presentation/presenter/MainMapPresenter.java
@@ -0,0 +1,14 @@
+package com.example.OKM.presentation.presenter;
+
+import com.example.OKM.presentation.view.MainMapActivity;
+
+/**
+ * Created by kubut on 2015-07-12.
+ */
+public class MainMapPresenter {
+    private MainMapActivity mainMapActivity;
+
+    public MainMapPresenter(MainMapActivity mainMapActivity){
+        this.mainMapActivity = mainMapActivity;
+    }
+}
---
+++ src/com/example/OKM/presentation/view/MainMapActivity.java
@@ -0,0 +1,31 @@
+package com.example.OKM.presentation.view;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import com.example.OKM.R;
+
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * Created by kubut on 2015-07-12.
+ */
+public class MainMapActivity extends FragmentActivity implements OnMapReadyCallback{
+    private SupportMapFragment map;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_map_activity);
+
+        map = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        map.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+
+    }
+}
