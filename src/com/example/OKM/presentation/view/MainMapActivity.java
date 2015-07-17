9f18d285f061c3cc0f058a2de6fbb945650347fb
jakubgorowski <fotokubut@gmail.com>
2015-07-18 01:43:40 +0200

    added support v7 (appcompat)

--- src/com/example/OKM/presentation/view/MainMapActivity.java
+++ src/com/example/OKM/presentation/view/MainMapActivity.java
@@ -24,7 +24,6 @@ public class MainMapActivity extends FragmentActivity implements OnMapReadyCallb
         map.getMapAsync(this);
     }
 
-    @Override
     public void onMapReady(GoogleMap map) {
 
     }
