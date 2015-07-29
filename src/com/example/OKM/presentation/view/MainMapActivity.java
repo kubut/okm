b74ae8d688b438337d4bd1ccb328b4d69bee8e41
jakubgorowski <fotokubut@gmail.com>
2015-07-29 20:41:16 +0200

    - Gradle basic configuration - drawerLayout basic configuration

--- src/com/example/OKM/presentation/view/MainMapActivity.java
+++ src/com/example/OKM/presentation/view/MainMapActivity.java
@@ -1,27 +1,68 @@
 package com.example.OKM.presentation.view;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import com.example.OKM.R;
 
 import com.google.android.gms.maps.*;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by kubut on 2015-07-12.
  */
-public class MainMapActivity extends FragmentActivity implements OnMapReadyCallback{
+public class MainMapActivity extends AppCompatActivity implements OnMapReadyCallback{
     private SupportMapFragment map;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_map_activity);
 
-        map = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        map.getMapAsync(this);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mTitle = getTitle();
+        mDrawerTitle = "Navigation Drawer";
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.main_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                toolbar,
+                R.string.drawer_open,
+                R.string.drawer_close)
+        {
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                toolbar.setTitle(mTitle);
+                invalidateOptionsMenu();
+                syncState();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                toolbar.setTitle(mDrawerTitle);
+                invalidateOptionsMenu();
+                syncState();
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+
+//        map = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        map.getMapAsync(this);
     }
 
     public void onMapReady(GoogleMap map) {
